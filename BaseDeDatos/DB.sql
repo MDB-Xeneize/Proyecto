2d791cb1e0b2e36346ef27e7330041fa0cafeeaa
Betancur Mariano <125041706+MDB-Xeneize@users.noreply.github.com>
2023-08-26 11:29:13 -0300

    Update DB.sql

--- BaseDeDatos/DB.sql
+++ BaseDeDatos/DB.sql
@@ -1,56 +1,55 @@
 CREATE DATABASE backend;
 
-
 CREATE TABLE CHOFER (
-  dni INT NOT NULL,
-  ip_chofer INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  dni INT NOT NULL UNIQUE,
+  id_chofer INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   apellido_nombre VARCHAR(20),
   fecha_nacimiento DATE,
   edad INT,
   carnet VARCHAR(20)
-  );
+);
+
 
 CREATE TABLE CAMIONES (
-  patente VARCHAR(20) NOT NULL,
-  id_camion INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  patente VARCHAR(20) NOT NULL ,
+  id_camion INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   marca VARCHAR(20),
   capacidad_carga INT,
   modelo VARCHAR(20),
-  peso_camion INT  
+  peso_camion INT
 );
 
 CREATE TABLE ENTRADA (
   peso_entrada INT,
   carga VARCHAR(20),
-  id_entrada INT NOT NULL primary key AUTO_INCREMENT,
-  hora_entrada INT,
+  id_entrada INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  hora_entrada TIME,
   fecha_entrada DATE,
-  id_chofer INT NOT NULL UNIQUE,
-  id_viaje INT NOT NULL UNIQUE,
-  id_camion INT NOT NULL UNIQUE,
+  id_chofer INT NOT NULL,
+  id_viaje INT NOT NULL,
+  id_camion INT NOT NULL,
   FOREIGN KEY (id_camion) REFERENCES CAMIONES(id_camion),
-  FOREIGN KEY (id_chofer) REFERENCES CHOFER(id_chofer)
+  FOREIGN KEY (id_chofer) REFERENCES CHOFER(dni)
 );
 
 CREATE TABLE SALIDA (
   destino VARCHAR(20),
   peso_salida INT,
   fecha_salida DATE,
-  hora_salida INT,
+  hora_salida TIME,
   carga VARCHAR(20),
-  id_salida INT NOT NULL primary key AUTO_INCREMENT,
-  id_chofer INT NOT NULL UNIQUE,
-  id_camion INT NOT NULL UNIQUE,
+  id_salida INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  id_chofer INT NOT NULL,
+  id_camion INT NOT NULL,
   FOREIGN KEY (id_camion) REFERENCES CAMIONES(id_camion),
-  FOREIGN KEY (id_chofer) REFERENCES CHOFER(id_chofer)
+  FOREIGN KEY (id_chofer) REFERENCES CHOFER(dni)
 );
 
 CREATE TABLE MANTENIMIENTO (
-  id_mantenimiento INT NOT NULL primary key AUTO_INCREMENT,
-  id_camion INT NOT NULL UNIQUE,
+  id_mantenimiento INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  id_camion INT NOT NULL,
   service_ultimo DATE,
   service_proximo DATE,
   FOREIGN KEY (id_camion) REFERENCES CAMIONES(id_camion)
-  
 );
 
