832f0dc8542218e2107175fdf318c539d871e8bc
Betancur Mariano <125041706+MDB-Xeneize@users.noreply.github.com>
2023-10-08 20:49:58 -0300

    Update index.js

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -1,40 +1,40 @@
 require('rootpath')();
 const express = require('express');
 const morgan = require('morgan');
-const app = express();
-var cors = require('cors')
+const cors = require('cors'); // Importar cors
 
-app.use(cors())
-//const router = express.Router();
-//app.use(express.static("public"));
+const app = express();
+
+app.use(cors()); // Habilitar CORS
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan('tiny'));
 morgan(':method :url :status :res[content-length] - :response-time ms');
 
 const configuracion = require("config.json");
-
-
 //const controladorPersona = require("controller/personaController.js");
 const controladorUsuario = require("controller/usuarioController.js");
-const controladorVehiculo = require("controller/vehiculoController.js");
 const controladorChofer = require("controller/choferController.js");
-const controladorInOut= require("controller/inOutController.js");
+const controladorVehiculo = require("controller/vehiculoController.js");
+const controladorMantenimiento = require("controller/mantenimientoController.js");
+const controladorTipo_Viaje = require("controller/tipo_ViajeController.js");
+const controladorViaje = require("controller/viajeController.js");
+const controladorGestiona = require("controller/gestionaController.js");
 
+// Rutas de controladores
 //app.use('/api/persona', controladorPersona);
 app.use('/api/usuario', controladorUsuario);
-app.use('/api/vehiculo', controladorVehiculo);
 app.use('/api/chofer', controladorChofer);
-app.use('/api/inOut', controladorInOut);
-
-
-//aplicacion --> cuando se ejecuta el use hace dos cosas
-//(todo lo que entre aca, enviamelo aca)
+app.use('/api/vehiculo', controladorVehiculo);
+app.use('/api/mantenimiento', controladorMantenimiento);
+app.use('/api/tipo_viaje', controladorTipo_Viaje);
+app.use('/api/viaje', controladorViaje);
+app.use('/api/gestiona', controladorGestiona);
 
 app.listen(configuracion.server.port, (err) => {
     if (err) {
         console.log(err);
     } else {
-        console.log("sevidor escuchando en el puerto " + configuracion.server.port);
+        console.log("Servidor escuchando en el puerto " + configuracion.server.port);
     }
 });
